254627be0a40bf8ce40976517c357e0c18f366cd
Henrik Mulari <77975379+henccadude@users.noreply.github.com>
2025-01-03 17:52:52 +0200

    Add files via upload

---
+++ dog-gallery/script.js
@@ -0,0 +1,74 @@
+//picture arrays
+
+const galleryImages = [
+  {
+    src: "dogpics/dogpic1.jpg",
+    alt: "dog picture 1"
+  },
+  {
+    src: "dogpics/dogpic2.jpg",
+    alt: "dog picture 2"
+  },
+  {
+    src: "dogpics/dogpic3.jpg",
+    alt: "dog picture 3"
+  },
+  {
+    src: "dogpics/dogpic4.jpg",
+    alt: "dog picture 4"
+  },
+  {
+    src: "dogpics/dogpic5.jpg",
+    alt: "dog picture 5"
+  },
+  {
+    src: "dogpics/dogpic6.jpg",
+    alt: "dog picture 6"
+  }
+];
+
+
+function menuHandler(gallery){
+  let gallerySection = document.querySelector(".gallery");
+
+  gallery.forEach(function(picture, index){
+    //element create
+    let picElm = document.createElement("div");
+    picElm.classList.add("gallery-item");
+    //pic create
+    let picImg = document.createElement("img");
+    picImg.classList.add("imgGallery");
+    picImg.src = picture.src;
+    picImg.alt = picture.alt;
+
+    //add pic to elment
+    picElm.append(picImg);
+
+    //show element
+    gallerySection.append(picElm)
+
+  });
+  
+
+};
+
+/*<div class="gallery-item">
+      <img class="img" src="dogpics/dogpic1.jpg" alt="picture">
+    </div>
+    <div class="gallery-item">
+      <img class="img" src="dogpics/dogpic2.jpg" alt="picture">
+    </div>
+    <div class="gallery-item">
+      <img class="img" src="dogpics/dogpic3.jpg" alt="picture">
+    </div>
+    <div class="gallery-item">
+      <img class="img" src="dogpics/dogpic4.jpg" alt="picture">
+    </div>
+    <div class="gallery-item">
+      <img class="img" src="dogpics/dogpic5.jpg" alt="picture">
+    </div>
+    <div class="gallery-item">
+      <img class="img" src="dogpics/dogpic6.jpg" alt="picture">
+</div>*/
+
+menuHandler(galleryImages);
